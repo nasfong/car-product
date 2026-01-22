19ff1e24844a50f80059a6c04d73c33b31f92be4
Samnang Tangfong <samnang.tangfong@DESTANGFONG.sabay.office>
2026-01-22 15:03:16 +0700

    update

--- app/cars/[id]/page.tsx
+++ app/cars/[id]/page.tsx
@@ -45,12 +45,6 @@ export async function generateMetadata({
         },
       ],
     },
-    twitter: {
-      card: 'summary_large_image',
-      title: car.name,
-      description: `$${car.price} - ${car.transmission}, ${car.fuelType}`,
-      images: [ogImageUrl],
-    },
   };
 }
 
